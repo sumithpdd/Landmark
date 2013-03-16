bcc0a756d14d6a1575f3638fe5495852f3bc3a4c
Sumith <sumith.damodaran@thebioagency.com>
2013-03-16 16:09:35 +0000

    First checkin

---
+++ Landmark.Dev4Good.WP/Landmark.Dev4Good.WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Landmark.Dev4Good.WP.Resources;
+
+namespace Landmark.Dev4Good.WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
