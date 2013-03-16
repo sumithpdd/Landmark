1af137178c9e307823d210d9978e55817cd0f31d
George Artemiou <giorgosart@hotmail.com>
2013-03-16 20:04:28 +0000

    added GetLandmarksForLocation();

--- Landmark.Dev4Good.WP/Landmark.Dev4Good.WP/MapView.xaml.cs
+++ Landmark.Dev4Good.WP/Landmark.Dev4Good.WP/MapView.xaml.cs
@@ -25,6 +25,8 @@ namespace Landmark.Dev4Good.WP
         {
             InitializeComponent();
         }
+
+        //main method
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
@@ -37,11 +39,14 @@ namespace Landmark.Dev4Good.WP
                     LocationPanel.Visibility = Visibility.Collapsed;
                     BuildApplicationBar();
                     GetCurrentCoordinate();
-               
+                    GetLandmarksForLocation();
+
             }
 
             DrawMapMarkers();
         }
+
+      
 
         /// <summary>
         /// Event handler for location usage permission at startup.
@@ -590,6 +595,11 @@ namespace Landmark.Dev4Good.WP
                 MessageBox.Show(AppResources.LocationDisabledMessageBoxText, AppResources.ApplicationTitle, MessageBoxButton.OK);
             }
             HideProgressIndicator();
+        }
+
+        private void GetLandmarksForLocation()
+        {
+            
         }
 
         /// <summary>
