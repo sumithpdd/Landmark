ae51aeb94cc937f0adc694b5e33b835851a0dd0a
Sumith <sumith.damodaran@thebioagency.com>
2013-03-17 03:39:49 +0000

    Fixing last pages

---
+++ Landmark.Dev4Good.WP/Landmark.Dev4Good.WP/Congrats.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Landmark.Dev4Good.WP
+{
+    public partial class Congrats : PhoneApplicationPage
+    {
+        public Congrats()
+        {
+            InitializeComponent();
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+    }
+}
